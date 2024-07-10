78262f00404df0001e81b2471011c39aa38d37a7
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-09 21:09:41 -0500

    jQuery

---
+++ index.js
@@ -0,0 +1,10 @@
+// jQuery()
+
+// Adding class
+$("h1").addClass("big-title margin-50");
+
+// Adding text
+$("button").text("Don't click me")
+
+// Adding HTML, could be text only also
+$("button").html("<em>Hey</em>");
