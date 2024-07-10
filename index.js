a62d15c5546c4b634f922cd627fb5444e59b65ce
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-09 23:42:38 -0500

    jQuerry Animations

--- index.js
+++ index.js
@@ -36,5 +36,25 @@
 // });
 
 // 4) Adding and Removing elements with jQuery
-$("h1").before("<button>New added button</button>")
-$(document).remove("h1")
+// .before() adds element before given
+// .after() adds element after given
+// .prepend() adds element before the content of the given element
+// .append() adds element after the content of the given element
+// $("h1").before("<button>New added button</button>")
+// $(document).remove("button")
+
+
+// 5) Website Animations
+// hide() makes element to be not displayed
+// toggle() makes element element to hide and unhide if listener was repeatedly triggered
+// fadeOut() smoothly makes element to be not displayed
+// fadeIn() smoothly makes visisble element which is not displayed
+// fadeToggle() combination of fadeOut() and fadeIn()
+// slideUp() slides up the element, makes it not visible
+// slideDown() slides down the element, makes it visible
+// animate() - here you can use only those styles which use numbers
+// slideUp().slideDown().animate({margin-top:20}) - we can make a sequence of animations
+$("button").on("click", function(){
+    $("h1").animate({opacity: 0.5});
+});
+
