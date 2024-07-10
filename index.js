c958fdf5fa3877582e67f659a13e63efca62ddb0
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-09 22:42:26 -0500

    Adding Event Listeners with jQuery

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-// jQuery()
-
+// 1) jQuery
 // Adding class
-$("h1").addClass("big-title margin-50");
+// $("h1").addClass("big-title margin-50");
 
-// Adding text
-$("button").text("Don't click me")
+// // Adding text
+// $("button").text("Don't click me")
 
-// Adding HTML, could be text only also
-$("button").html("<em>Hey</em>");
+// // Adding HTML, could be text only also
+// $("button").html("<em>Hey</em>");
 
-// Printing a value of the attribute of img
-console.log($("img").attr("src"));
+// // Printing a value of the attribute of img
+// console.log($("img").attr("src"));
 
-// Setting a value for the attribute of img
-$("a").attr("href", "https://www.yahoo.com");
+// // Setting a value for the attribute of img
+// $("a").attr("href", "https://www.yahoo.com");
 
+// 2) Addig CSS
+// $("h1").css("color","green");
 
+// 3) EventListeners
+// Adding click Event Listener
+// $("h1").on("click",function(){
+//     $("h1").css("color","purple");
+//     console.log("You clicked the button)");
+// });
 
+// // Adding Event Listener to multiple elements
+// $("button").on("click", function(){
+//     $("a").css("color","blue");
+//     $("a").css("font-size","3rem");
+// });
+
+// Triggering change of the value of h1 tag
+$("body").on("keypress", function(event){
+    $("h1").text(event.key);
+});
