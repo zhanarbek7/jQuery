54ffbef2d2a6a8f6d00f474307da702cceecd860
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-07-09 21:33:08 -0500

    Manipulating attributes with jQuery

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ $("button").text("Don't click me")
 
 // Adding HTML, could be text only also
 $("button").html("<em>Hey</em>");
+
+// Printing a value of the attribute of img
+console.log($("img").attr("src"));
+
+// Setting a value for the attribute of img
+$("a").attr("href", "https://www.yahoo.com");
+
+
+
